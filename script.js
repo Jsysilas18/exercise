e9b39514d9b52453e677717ed46c74f2ce489762
silas Jesse <51911528+Jsysilas18@users.noreply.github.com>
2019-06-17 06:20:51 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+/*var database = [
+    { 
+      username: "jess",
+      password: "jessejones"
+    }
+];
+
+var newsFeed = [ 
+    { 
+    	username: "nessa",
+	    timeline: "javascript is so cool"
+    },
+    {
+    	username: "Bobby",
+    	timeline: "so tired from all the learning!"
+    }
+ ]; 
+var userNamePrompt = prompt("what`s your username?");
+var passwordPrompt = prompt("what`s your password?");
+
+function signIn(username, password) {
+ 	if (username === database[0].username &&  
+ 		password === database[0].password)
+ 		 {
+ 		console.log(newsFeed);
+ 	} else {
+ 		alert("sorry,  wrong username and password!");
+ 	}
+ }
+ signIn(userNamePrompt,passwordPrompt);*/
